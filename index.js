89912d5e06227396c0ae2adf944af1ed359de68d
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2023-06-27 15:18:19 +0200

    La til første config

---
+++ index.js
@@ -0,0 +1,26 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+    "next/core-web-vitals",
+    "@navikt/teamia-a11y",
+    "prettier", // Add "prettier" last. This will turn off eslint rules conflicting with prettier. This is not what will format our code.
+  ],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/no-empty-function": "off",
+  },
+  overrides: [
+    {
+      files: ["**/?(*.)+(spec|test).[jt]s?(x)"],
+      extends: ["plugin:testing-library/react"],
+      rules: {
+        "testing-library/no-debugging-utils": "warn",
+      },
+    },
+  ],
+  settings: {},
+  ignorePatterns: ["*.module.css"],
+};
