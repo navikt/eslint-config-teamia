0f713326a50a4e574af35ed4d350e0e27a4255d2
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2023-10-31 14:33:13 +0100

    Fjernet dependency til eslint-config-teamia-a11y. La til regel fra eslint-plugin-jsx-a11y i dette regelsettet.

--- index.js
+++ index.js
@@ -5,12 +5,32 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:jest/recommended",
     "next/core-web-vitals",
-    "@navikt/teamia-a11y",
     "prettier", // Add "prettier" last. This will turn off eslint rules conflicting with prettier. This is not what will format our code.
   ],
   rules: {
     "@typescript-eslint/no-unused-vars": "error",
     "@typescript-eslint/no-empty-function": "off",
+    "jsx-a11y/anchor-ambiguous-text": [
+      2,
+      {
+        words: [
+          "her",
+          "klikk",
+          "klikk her",
+          "trykk",
+          "trykk her",
+          "lenken",
+          "linken",
+          "lenka",
+          "lenken her",
+          "linken her",
+          "lenka her",
+          "denne lenken",
+          "denne linken",
+          "denne lenka",
+        ],
+      },
+    ],
   },
   overrides: [
     {
